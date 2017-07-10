0e12ce668eae9716f4fbfd3cd09479db64f7a631
leon <leon@leons-MacBook-Pro.local>
2017-07-10 18:54:16 +0800

    fix

---
+++ apps/src/util/sharedConstants.js
@@ -0,0 +1,23 @@
+// This is a generated file and SHOULD NOT BE EDITTED MANUALLY!!
+// Contents are generated as part of grunt build
+// Source of truth is lib/cdo/shared_constants.rb
+
+export const LevelKind = {
+  "peer_review": "peer_review",
+  "assessment": "assessment",
+  "puzzle": "puzzle",
+  "unplugged": "unplugged",
+  "level": "level"
+};
+
+export const LevelStatus = {
+  "not_tried": "not_tried",
+  "submitted": "submitted",
+  "locked": "locked",
+  "perfect": "perfect",
+  "passed": "passed",
+  "attempted": "attempted",
+  "review_accepted": "review_accepted",
+  "review_rejected": "review_rejected",
+  "dots_disabled": "dots_disabled"
+};
