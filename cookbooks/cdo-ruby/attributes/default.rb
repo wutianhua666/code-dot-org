1684b7e745f5e6d7f2b9cc95a5e241f68f5c30fe
Will Jordan <will@code.org>
2017-10-04 10:59:22 -0700

    upgrade cdo-ruby dependencies [ci skip]

--- cookbooks/cdo-ruby/attributes/default.rb
+++ cookbooks/cdo-ruby/attributes/default.rb
@@ -1,6 +1,6 @@
 default['cdo-ruby'] = {
   version: '2.2',
-  rubygems_version: '2.4.8',
-  bundler_version: '1.10.6',
-  rake_version: '10.5.0'
+  rubygems_version: '2.6.13',
+  bundler_version: '1.16.0.pre.2',
+  rake_version: '12.1.0'
 }
